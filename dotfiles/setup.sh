0bc57c5afacc4f11c82787663e919152cf267850
Jasper Van den Broeck <137291842+JasperBroeck@users.noreply.github.com>
2024-07-23 13:38:56 +0200

    Add files via upload

---
+++ dotfiles/setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash 
+cd
+mkdir Downloads
+
+# Install programs using pacman
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm flatpak git curl wget btop nautilus kitty bluez bluez-utils blueman starship fastfetch nm-connection-editor networkmanager network-manager-applet netctl dialog gnome-text-editor grim slurp waybar wofi hyprland
+
+# Install AUR helper
+sudo pacman -S --noconfirm --needed base-devel git
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+rm -rf paru
+source /etc/profile
+sudo pacman -Syu --noconfirm
+
+# Install nwg-look-bin
+paru -S nwg-look-bin
+
+# Install flatpak apps
+flatpak install flathub com.microsoft.Edge -y
+flatpak install flathub md.obsidian.Obsidian -y
+flatpak install flathub io.gitlab.idevecore.Pomodoro -y
+flatpak install flathub org.audacityteam.Audacity -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub org.kde.kdenlive -y
+flatpak install flathub org.libreoffice.LibreOffice -y
+flatpak install flathub com.mojang.Minecraft -y
+flatpak install flathub io.github.ec_.Quake3e.OpenArena -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.vscodium.codium -y
+
+# Install theme, icons and cursors
+cd ~/Downloads
+git clone https://github.com/JasperBroeck/Miaj-Dotfiles
+git clone https://github.com/vinceliuice/Graphite-gtk-theme
+git clone https://github.com/vinceliuice/Fluent-icon-theme/
+paru -S bibata-cursor-theme-bin
+
+unzip Fluent-icon-theme-master.zip
+unzip Graphite-gtk-theme-main.zip
+unzip Miaj-Dotfiles-main.zip
+
+cd Fluent-icon-theme
+./install.sh
+cd ..
+
+cd Graphite-gtk-theme
+./install.sh
+cd ..
+
+# System Installation
+cd Miaj-Dotfiles
+cd dotfiles
+cp -r wallpaper/ ~/wallpaper/
+cp .bashrc ~/.bashrc
+cd .config/
+rm -r ~/.config/hypr/
+cp -r hypr/ ~/.config/hypr/
+rm -r ~/.config/fastfetch/
+cp -r fastfetch/ ~/.config/fastfetch/
+rm -r ~/.config/kitty/
+cp -r kitty/ ~/.config/kitty/
+rm -r ~/.config/waybar/
+cp -r waybar/ ~/.config/waybar/
+rm -r ~/.config/wofi/
+cp -r wofi/ ~/.config/wofi/
+rm ~/.config/starship.toml
+cp starship.toml ~/.config/starship.toml
+
+cd
+
+sudo reboot
