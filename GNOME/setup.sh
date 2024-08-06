4fb7c7b6020bed0d94afaa583894302bb66ac6fb
Jasper Van den Broeck <137291842+JasperBroeck@users.noreply.github.com>
2024-08-06 10:03:00 +0200

    Update setup.sh

--- GNOME/setup.sh
+++ GNOME/setup.sh
@@ -1,37 +1,32 @@
 #!/bin/bash 
-cd
-mkdir Downloads
-
 # Install programs using pacman
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm tlp-rdw hyprpaper flatpak git curl wget btop jdk-openjdk nautilus kitty bluez bluez-utils blueman starship fastfetch nm-connection-editor networkmanager network-manager-applet netctl dialog gnome-text-editor grim slurp waybar wofi hyprland
-sudo pacman -Rns dolphin
+sudo pacman -S --noconfirm tlp-rdw flatpak git curl wget btop jdk-openjdk starship fastfetch wofi obsidian freecad 
+
 # Install AUR helper
 sudo pacman -S --noconfirm --needed base-devel git
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
-rm -rf paru
-source /etc/profile
 sudo pacman -Syu --noconfirm
 
 # Install programs using paru
 paru -S minecraft-launcher
-paru -S nwg-look-bin
 paru -S balena-etcher
 paru -S woeusb
+paru -S google-chrome
+paru -S spotify
+
+# Install Waveform 12.5
+
 
 # Install flatpak apps
-flatpak install flathub com.microsoft.Edge -y
-flatpak install flathub md.obsidian.Obsidian -y
-flatpak install flathub io.gitlab.idevecore.Pomodoro -y
 flatpak install flathub org.audacityteam.Audacity -y
 flatpak install flathub com.obsproject.Studio -y
 flatpak install flathub org.kde.kdenlive -y
 flatpak install flathub org.libreoffice.LibreOffice -y
 flatpak install flathub io.github.ec_.Quake3e.OpenArena -y
-flatpak install flathub com.spotify.Client -y
 flatpak install flathub com.vscodium.codium -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.valvesoftware.Steam -y
@@ -46,7 +41,6 @@ paru -S bibata-cursor-theme-bin
 
 unzip Fluent-icon-theme-master.zip
 unzip Graphite-gtk-theme-main.zip
-unzip Miaj-Dotfiles-main.zip
 
 cd Fluent-icon-theme
 ./install.sh
